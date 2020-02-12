bb34b3044a00f6e1579386d0f1f3ee78a7380689
Marco Sánchez <52178350+mszsorondo@users.noreply.github.com>
2020-02-12 12:36:43 -0300

    Add files via upload

---
+++ toying.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec 18 07:37:03 2019
+
+@author: Marco Sanchez S
+"""
+
+ class matrix():
+     def __init__(self, rows, columns):
+         self.rows = rows;
+         self.columns = columns;
+    def scalProduct(scal=1):
+        pass;
+    def suMat(mat):
+        pass;
+    def invertMat():
+        pass;
+    
+    
+mat1 = matrix(2,2) 
+mat1.scalProduct(7)   
+
+aDict = {
+        'a' : 1 , 8 : [True, 7 , 'Good Morning']}
+aDict.keys()    
+
+
+strings = ['a', 'as', 'bat', 'car', 'dove', 'python']
+strings2 = [];
+for k in strings:
+    if (length(k)>2):
+        k = k.upper()
+        strings2.append(k);
+            
+        
